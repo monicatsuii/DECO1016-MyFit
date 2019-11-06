09d16cbd8dabfad521e41203e21a5ae6e9e83cef
monicatsuii <57380734+monicatsuii@users.noreply.github.com>
2019-11-07 01:02:47 +1100

    add more pages to signup

---
+++ Sign Up/signup_script.js
@@ -0,0 +1,23 @@
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn1");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}
+
+var header = document.getElementById("myDIV");
+var btns2 = header.getElementsByClassName("btn2");
+for (var i = 0; i < btns2.length; i++) {
+  btns2[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}
